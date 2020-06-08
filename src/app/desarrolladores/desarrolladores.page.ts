78174af9e34642e8cc4b1c26160e158ad57cb8b2
Jose Luis Flores Garcia <josefloresgarcia17@gmail.com>
2020-06-08 16:40:59 -0500

    web functionality

--- src/app/desarrolladores/desarrolladores.page.ts
+++ src/app/desarrolladores/desarrolladores.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import axios from "axios";
 @Component({
   selector: 'app-desarrolladores',
   templateUrl: './desarrolladores.page.html',
@@ -8,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class DesarrolladoresPage implements OnInit {
 
   constructor() { }
-
+  usuarios: any;
   ngOnInit() {
+    this.getProfiles();
+
+  }
+  async getProfiles(){
+    try {
+      const res = await axios.get("http://localhost:5000/api/profile");
+      this.usuarios = res.data;
+      console.log(res);
+  } catch (err) {
+      console.log(err);
+  }
+  }
+
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+    this.getProfiles();
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
   }
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import axios from "axios";
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  user = {
+    name:"",
+    password: ""
+  };
   ngOnInit() {
   }
 
+  async login(){
+    const config = {
+      headers: {
+          "Content-Type": "application/json"
+      }
+  };
+    console.log(this.user);
+    var email = this.user.name;
+    var password = this.user.password;
+     const body = JSON.stringify({email, password});
+   try{
+    
+    const res = await axios.post("http://localhost:5000/api/auth", body, config);
+    console.log("respuesta: ");
+    console.log(res);
+    // routerLink="/inicio"
+    const router = document.querySelector('ion-router');
+    const routeRedirect = document.createElement
+    if(res != null){
+        this.router.navigate(["/desarrolladores"]);
+    }
+   } catch(err){
+    console.log(err);
+   }
+  }
 }
